eadca33a8d33ec9f3af0d31c8b612c5431466ea8
ahmedqac <ahmed@opslabahmed3.noc.cpwn.lan>
2020-02-18 15:39:41 +0000

    added selenium testing

---
+++ selenium_testing.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+import unittest
+
+class DjangoFunctionalCase(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def test_first(self):
+        self.browser.get('http://google.com')
+        assert self.browser.page_source.find('google')
+    
+    def tearDown(self):
+        self.browser.quit()
+
+# browser = webdriver.Firefox()
+# browser.get('http://google.com')
+
+# assert browser.page_source.find('google')
+
+if __name__ == '__main__':
+    unittest.main()
